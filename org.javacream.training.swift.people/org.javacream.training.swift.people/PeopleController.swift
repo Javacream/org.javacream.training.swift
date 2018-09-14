77e8c60e819305947c19080b96930e8f1ad86554
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-09-14 09:14:30 +0200

    add post http example

--- org.javacream.training.swift.people/org.javacream.training.swift.people/PeopleController.swift
+++ org.javacream.training.swift.people/org.javacream.training.swift.people/PeopleController.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+extension Person{
+    var toJson: String {
+        let dict = ["lastname" : lastname, "firstname" : firstname, "height": height] as [String : Any]
+        let data =  try! JSONSerialization.data(withJSONObject: dict, options: [])
+        return String(data:data, encoding:.utf8)!
+    }
+}
+
+
 class PeopleController{
     var peopleModel: PeopleModel!
     func asyncSearchPeopleByLastname (lastname: String, update: @escaping (Array<String>) -> Void) -> Void{
@@ -18,11 +27,22 @@ class PeopleController{
     }
     
     func asyncSavePerson(lastname: String, firstname:String, height: Int, update: @escaping (Person) -> Void, error: @escaping (Error) -> Void){
-        
+        func saveServer() -> Person{
+            let p =  Person(lastname: lastname, firstname: firstname, height: height)
+            let url = URL(string: "http://10.28.7.100:9090/people")
+            var request = URLRequest(url: url!)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody=p.toJson.data(using: .utf8)
+            let urlSession = URLSession.shared
+            let task = urlSession.dataTask(with: request)
+            task.resume()
+            return p
+        }
         func background() throws -> Person{
             return try PeopleApplicationContext.peopleModel.savePerson(lastname: lastname, firstname: firstname, height: height)
         }
         
-        background ->>- (update, error)
+        saveServer ->>- (update, error)
     }
 }
